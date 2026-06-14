4cafe705b1a6fba7835050b6f6bdbb095495aaff
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-14 18:32:25 +0300

    Added full code logic

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-enter = int(input("Enter number: "))
-usd = 72.42  #rub
+# 1. Сначала объявляем курсы валют
+usd = 72.42
+eur = 83.83
 
+# 2. Объявляем функции для расчета (переменные вводим ВНЕ функций)
 def rub_to_usd(amount):
     try:
-        number = amount / usd
-        return number
+        return amount / usd
     except ZeroDivisionError:
         return "The USD exchange rate cannot be zero."
 
-result = rub_to_usd(enter)
-print(result)
+def rub_to_eur(amount):
+    try:
+        return amount / eur
+    except ZeroDivisionError:
+        return "The EUR exchange rate cannot be zero."
 
