11649d0fbd8310dc2661d07c4c0700ff98ce236c
ArdenchikPlus <raskovalov132@gmail.com>
2026-06-14 18:33:41 +0300

    last version

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# 1. Сначала объявляем курсы валют
 usd = 72.42
 eur = 83.83
 
-# 2. Объявляем функции для расчета (переменные вводим ВНЕ функций)
 def rub_to_usd(amount):
     try:
         return amount / usd
@@ -15,3 +13,15 @@ def rub_to_eur(amount):
     except ZeroDivisionError:
         return "The EUR exchange rate cannot be zero."
 
+print("1. Convert to USD:\n2. Convert to EUR:")
+action = int(input("Enter action(number): "))
+
+if action == 1:
+    enter = int(input("Enter number: "))
+    result = rub_to_usd(enter)
+    print(result)
+
+elif action == 2:
+    enter = int(input("Enter number: "))
+    result = rub_to_eur(enter)
+    print(result)
